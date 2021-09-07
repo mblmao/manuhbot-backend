10dfc314d652a0edbe9e6cc8e20577ed54b74f82
mblmao <65665790+mblmao@users.noreply.github.com>
2021-09-07 16:56:08 -0300

    Add files via upload
    Adding "config folder"

---
+++ config/config.js
@@ -0,0 +1,15 @@
+
+const dialogflowConfig =   {
+  "type": "service_account",
+  "project_id": "manubot-nob9",
+  "private_key_id": "efcddaf29a815f7915a762728dcbc1fdd4501fbc",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC/IQg1ibq3BaIH\nRGR+z/kpv2gNj7NqHb6I+cx2Tzu+KKevwXGTs5aEyhTgLaKgjh5xKJHG0OEQJt9C\naYS3lEFV4ytSbFguISxRCEHHkXDJLlDxNwB8q6EafkoZGeDebrALpdzjmDW2Ud8m\n3hOOHwBLwOdg97Ep8kmzCERw0PPELM/0bEiEgMHSZbxTLt2oWe2YoD7Scvgo90Bt\nlBAF3vsqyOQPh0MIveoi63rLMIneG5SYEWlx2Q98S32qbLiNT2WVve2ZFep0UsPL\nnFTpoCQaXczp/qZSGUiye/ZVNEFEgBBI1OGFfAM3Nriox+vOwizX2EQI/ijzRAWB\nHGQ0MpdlAgMBAAECggEADcm8awmpD8asa4QfuyrrXtDL7l0wBGyUkBLJO7234cjS\ntAdWAgxCGbPvBpnJh3Xz30DzJ0RuqrjLLAzVrLKuy0Xh8bEW5BkI4T2hv9u6UbZx\nfhKe39+NII6nb3Z3oO7w7J4wuGCE0MbI55JnbGjIdNk74o8WgkqpeMIUweNpD9Kg\nV6L6IebkLxLYDZIdmLEBQ2UUqymy59T6uIDfNVicv57CmZ84UPneHK3kss6MWEQ3\n5zvfROAWtDTetDFrxNQ/+Wuefm3l2ZusKWe2wFfMOcteYguNcaR5VQGxERFsMUFb\nze+aYeIa2EMCebxLQrkp9a0Y45+v+2fuxLbOnMEL3wKBgQD2VD2v8I+5Fw+am8D2\nRD05FHaseVquVk4UZcNQgXDCQHnCeqEPhDnRDtxdhXfm83zbo/by/pCijmpGJECF\noH6G9Afzww25o5X5a4dqgo18OGH5h+zq2WjgIJwIMzTKz+cpYloueV/9fHLfHgMI\nzx/cI1pOzoeWzNVL76pP0DeJLwKBgQDGof8oRg7mmk4VfisV/tCad7528fzALbBa\nSpnr4RrEWbCcl8mti9of2m/WWK/BzLDsxCQS3mvaB4FqueK9sP2yQfbQJiGEPPoZ\nDmzTFH7SEU/9AAMDikX9+064jIP3JS2kRiUOmjFoRFyM+31LhSy5ZhMO77n/K8Cq\n5TCGif4bqwKBgGutV2nZVnoqQn/rJeaA3eJdt6KdSa57u5XfBgnjC09er9r7j/wg\naMWiIWIgPdGPDj1U51ZBQShN6NbQcgP3rFeBxRghY5aKk7x77K36c66hVvKlIENo\n1hdjKdHL0H/3caz9eT5ngDZmeDBgBiOLpvmr2oYocczRrUuMcH6DHkOxAoGACmhu\noEObgEXRlZq40zw43f8sQbtITSGXvgNusUdoSaWRdYMWlDthwI2Hkqmu++6nzqAw\na7Sv3grKozb+RIZDpNWEA0AcPXucdzekSCMoygUSNeWMQuE57ab/Zvgl6gtOqu7p\n/iviLt52IwUCebSOtWA922iATHiUbd/4fPQ6FmUCgYBR1vom+UU0Ekxw3+bty6V6\noGBH4pMZWEz1XE04UZmbQIYXFwlV30Q5K2XkcW3o4vmP3ORr6v9H7ewEYHspUaCJ\n+eNO8uilxgZ4SpFhfQCI6ZrQuga7eTdHB/yxqZPmvz+iwnbBEuXe2cxQe7lvJY6w\npRchXqAwTofm3uqeUzomiA==\n-----END PRIVATE KEY-----\n",
+  "client_email": "manuhbot-svc@manubot-nob9.iam.gserviceaccount.com",
+  "client_id": "110027532245654489991",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/manuhbot-svc%40manubot-nob9.iam.gserviceaccount.com"
+}
+    
+module.exports = dialogflowConfig
